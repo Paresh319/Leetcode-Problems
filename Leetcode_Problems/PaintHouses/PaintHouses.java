337900864518747257afa9f6e455c70e231c384a
Paresh Pandit <paresh.pandit3193@gmail.com>
2022-01-26 09:20:35 -0700

    Create PaintHouses.java

---
+++ Leetcode_Problems/PaintHouses/PaintHouses.java
@@ -0,0 +1,44 @@
+class Solution {
+    int[][] memo; 
+    public int minCost(int[][] costs) {
+        memo = new int[costs.length + 1][costs[0].length+1];
+        for(int[] l: memo)
+        {
+            Arrays.fill(l, -1);
+        }
+        
+        return Math.min(helper(costs, 0, 0), Math.min(helper(costs, 0, 1), helper(costs, 0, 2)));
+    }
+    
+    public int helper(int[][] costs, int row, int col)
+    {
+        if(memo[row][col] != -1)
+        {
+            return memo[row][col];
+        }
+        if(row > costs.length - 1 || col > 2)
+        {
+            return 0;
+        }
+        int o = costs[row][col];
+        // if(row <= costs.length - 1)
+        // {
+            
+        if(row == costs.length - 1){}
+        if(col == 0)
+        {
+            o +=  Math.min(helper(costs, row + 1, 1), helper(costs,row + 1, 2));
+        }
+        else if(col == 1)
+        {
+            o += Math.min(helper(costs, row + 1, 2), helper(costs, row + 1, 0));
+        }
+        else
+        {
+            o += Math.min(helper(costs, row + 1, 0), helper(costs, row + 1, 1));
+        }
+        
+        memo[row][col] = o;
+        return o;
+    }
+}
