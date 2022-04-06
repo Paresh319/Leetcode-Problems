e62e401cfd6be7f0ef5f52c21d363f341dcc25f4
Paresh Pandit <paresh.pandit3193@gmail.com>
2022-04-06 17:21:12 -0600

    Create LongestSubstringWithTwoUniqueCharacter.java

---
+++ Leetcode_Problems/LongestSubstringWithTwoUniqueCharacters/LongestSubstringWithTwoUniqueCharacter.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        
+        int i = 0; 
+        int j = 0;
+        int count = 0;
+        int maxLen = 0;
+        while(j < s.length())
+        {
+            hm.put(s.charAt(j), j);
+            if(hm.size() == 3)
+            {
+                int del = Collections.min(hm.values());
+                hm.remove(s.charAt(del));
+                i = del + 1;
+            }
+            j++;
+            maxLen = Math.max(maxLen, j - i);
+            
+        }
+        return maxLen;
+    }
+}
