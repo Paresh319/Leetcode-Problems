63e368c08f7e83cc7e10792f6b125c89c5cdf1a3
Paresh Pandit <paresh.pandit3193@gmail.com>
2022-04-06 17:12:05 -0600

    Create ValidTree.java

---
+++ Leetcode_Problems/ValidTree/ValidTree.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        
+        HashMap<Integer, List<Integer>> hm  = new HashMap<>();
+
+        for(int i = 0; i < n; i++)
+        {
+            hm.put(i, new ArrayList<Integer>());
+        }
+        for(int[] edge: edges)
+        {
+            hm.get(edge[0]).add(edge[1]);
+            hm.get(edge[1]).add(edge[0]);
+        }
+
+        if(dfs(hm, 0, hs, -1) && hs.size() == n){
+            return true;
+        }
+
+        return false;
+    }
+    
+   public boolean dfs(HashMap<Integer, List<Integer>> hm, int start, HashSet<Integer> hs, int parent)
+   {
+       if(hs.contains(start))
+       {
+           return false;
+       }
+       hs.add(start);
+
+       for(int a: hm.get(start))
+       {
+           if(parent != a) // this is important as we need to avoid trivial cycle since we came to that node from the parent but we will encountr that later as we have bi directional graph
+           {
+               if(!dfs(hm, a, hs, start))
+               {
+                   return false;
+               }
+           }
+           
+       }
+       return true;
+   }
+}
