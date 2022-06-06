78ecb63218058635680c1c4ad0961b09d9762461
ppandit5 <paresh.pandit3193@gmail.com>
2022-06-06 15:15:10 -0700

    minimum flips to monotone increasing

---
+++ Leetcode_Problems/MinimumFlipsToMonotoneIncreasing/MinimumFlipsToMonotoneIncreasing.java
@@ -0,0 +1,19 @@
+package Leetcode_Problems.MinimumFlipsToMonotoneIncreasing;
+
+public class MinimumFlipsToMonotoneIncreasing {
+    public int minFlipsMonoIncr(String s) {
+        
+        int one = 0;
+     int flip =0;
+     for(int i=0;i<s.length();i++)
+     {
+         if(s.charAt(i)=='1')
+             one++;
+         else
+             flip++;
+         flip = Math.min(one,flip);
+     }
+     return flip;
+         
+     }
+}
